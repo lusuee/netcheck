e2f56b7611117578e280174684bf2411195a28f8
lusuee <1349543950@qq.com>
2024-03-22 13:21:31 +0800

    Update netcheck.sh

--- netcheck.sh
+++ netcheck.sh
@@ -9,7 +9,7 @@ echo $BASE_PATH
 
 # 不需要额外的 shell 命令，比如 sh、nohup 等
 # 服务无法放到后台运行，该脚本执行结束，两个进程也会被杀掉
-${BASE_PATH}/soul-netcheck/endpoint/EndpointWatchdog.sh ${BASE_PATH} &
-${BASE_PATH}/soul-netcheck/tqclient/tqclient.sh ${BASE_PATH}
+${BASE_PATH}/netcheck/endpoint/EndpointWatchdog.sh ${BASE_PATH} &
+${BASE_PATH}/netcheck/tqclient/tqclient.sh ${BASE_PATH}
 
 echo "start success"
